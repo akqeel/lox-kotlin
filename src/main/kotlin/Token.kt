fe2fb676ecafa40b7392f4c18c776810f8811786
akqeel <akqeelnawaz@windowslive.com>
2022-06-25 16:37:11 +0800

    feat(lox/intepreter): add `TokenType`, `Token`

--- src/main/kotlin/Token.kt
+++ src/main/kotlin/Token.kt
@@ -1,2 +1,11 @@
-class Token {
+data class Token(
+    var type: TokenType? = null,
+    var lexeme: String? = null,
+    var literal: Any? = null,
+    var line: Int? = null,
+) {
+    override fun toString(): String {
+        return type.toString() + " " + lexeme + " " + literal
+    }
+
 }
