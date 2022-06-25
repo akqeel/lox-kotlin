78985f3ae854fdf8d644f1072d4c657f2a9eb8a2
akqeel <akqeelnawaz@windowslive.com>
2022-06-25 20:54:02 +0800

    feat(lox/parser): add `AstPrinter` class

--- src/main/kotlin/lox/AstPrinter.kt
+++ src/main/kotlin/lox/AstPrinter.kt
@@ -4,21 +4,49 @@ import lox.Expr.Visitor
 
 class AstPrinter: Visitor<String> {
 
+    companion object {
+        @JvmStatic fun main(args: Array<String>) {
+            val expression: Expr = Expr.Binary(
+                Expr.Unary(
+                    Token(TokenType.MINUS, "-", null, 1),
+                    Expr.Literal(123)
+                ),
+                Token(TokenType.STAR, "*", null, 1),
+                Expr.Grouping(
+                    Expr.Literal(45.67)
+                )
+            )
+            println(AstPrinter().print(expression))
+        }
+    }
+
     fun print(expr: Expr) = expr.accept(this)
 
-    override fun visitBinaryExpr(expr: Expr.Binary?): String {
-        TODO("Not yet implemented")
+    override fun visitBinaryExpr(expr: Expr.Binary): String {
+        return parenthesize(expr.operator.lexeme, expr.left, expr.right);
     }
 
-    override fun visitGroupingExpr(expr: Expr.Grouping?): String {
-        TODO("Not yet implemented")
+    override fun visitGroupingExpr(expr: Expr.Grouping): String {
+        return parenthesize("group", expr.expression);
     }
 
-    override fun visitLiteralExpr(expr: Expr.Literal?): String {
-        TODO("Not yet implemented")
+    override fun visitLiteralExpr(expr: Expr.Literal): String {
+        if (expr.value == null) return "nil";
+        return expr.value.toString();
     }
 
-    override fun visitUnaryExpr(expr: Expr.Unary?): String {
-        TODO("Not yet implemented")
+    override fun visitUnaryExpr(expr: Expr.Unary): String {
+        return parenthesize(expr.operator.lexeme, expr.right);
+    }
+
+    private fun parenthesize(name: String?, vararg exprs: Expr): String {
+        val builder = StringBuilder()
+        builder.append("(").append(name)
+        for (expr in exprs) {
+            builder.append(" ")
+            builder.append(expr.accept(this))
+        }
+        builder.append(")")
+        return builder.toString()
     }
 }
---
+++ src/main/kotlin/lox/Expr.kt
@@ -0,0 +1,36 @@
+package lox
+
+abstract class Expr {
+    interface Visitor<R> {
+        fun visitBinaryExpr(expr: Binary): R
+        fun visitGroupingExpr(expr: Grouping): R
+        fun visitLiteralExpr(expr: Literal): R
+        fun visitUnaryExpr(expr: Unary): R
+    }
+
+    class Binary internal constructor(val left: Expr, val operator: Token, val right: Expr) : Expr() {
+        override fun <R> accept(visitor: Visitor<R>): R {
+            return visitor.visitBinaryExpr(this)
+        }
+    }
+
+    class Grouping internal constructor(val expression: Expr) : Expr() {
+        override fun <R> accept(visitor: Visitor<R>): R {
+            return visitor.visitGroupingExpr(this)
+        }
+    }
+
+    class Literal internal constructor(val value: Any) : Expr() {
+        override fun <R> accept(visitor: Visitor<R>): R {
+            return visitor.visitLiteralExpr(this)
+        }
+    }
+
+    class Unary internal constructor(val operator: Token, val right: Expr) : Expr() {
+        override fun <R> accept(visitor: Visitor<R>): R {
+            return visitor.visitUnaryExpr(this)
+        }
+    }
+
+    abstract fun <R> accept(visitor: Visitor<R>): R
+}
